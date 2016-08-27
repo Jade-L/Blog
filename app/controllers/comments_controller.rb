f9349236460c72efcf077afeec8a8010637b1dd8
Jade-L <lemaissi.jade@gmail.com>
2016-08-27 21:42:16 +0200

    add delete params for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment_id])
+    @comment.save
+
+    redirect_to @posts
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to @comment.post
   end
 end
